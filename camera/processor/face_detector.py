6a2543df56f05845067fa23392a9e1f92e0ba11e
Takahumi Jonen <tj@TakahuminoMacBook-puro.local>
2018-09-04 19:20:23 +0900

    test04

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -42,5 +42,7 @@ class FaceDetector(object):
             #cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
 
+        cv2.putText(frame, len(face), (10,500), FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 2, cv2.LINE_AA)
+
         # frameを戻り値として返す
         return frame
